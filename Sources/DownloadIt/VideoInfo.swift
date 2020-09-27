88752f1556d98f1638b1946b23343dacae7b3853
Murilo Araujo <muernandes@icloud.com>
2020-09-27 00:45:11 -0300

    Fixed

--- Sources/DownloadIt/VideoInfo.swift
+++ Sources/DownloadIt/VideoInfo.swift
@@ -10,10 +10,10 @@ import Foundation
 typealias JSON = [String: Any]
 
 public struct VideoInfo: Codable, Hashable {
-    public var downloadUrl: String
-    public var title: String
-    public var videoExtension: String
-    public var fileSizeHumanReadable: String
+    public var downloadUrl: String?
+    public var title: String?
+    public var videoExtension: String?
+    public var fileSizeHumanReadable: String?
     public var height: Int?
     public var width: Int?
     public var fps: Int?
